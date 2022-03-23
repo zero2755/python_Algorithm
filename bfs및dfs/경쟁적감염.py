e01fe06789e3f1e7148584c02db8516a01678c30
zero2755 <56134028+zero2755@users.noreply.github.com>
2022-03-24 00:57:24 +0900

    Update 경쟁적감염.py

--- bfs및dfs/경쟁적감염.py
+++ bfs및dfs/경쟁적감염.py
@@ -1,3 +1,4 @@
+#다시
 N,K= map(int, input().split())
 
 from collections import deque
@@ -6,65 +7,40 @@ dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 
-
-
-
-
-
-
-
-
-
-
-'''
-잘못짠구현
 graph= []
 
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
+data=[]
 
-
-coin=[[] for _ in range(K+1)]
-
+#종류ㅜ 시간 위치
 for i in range(N):
+    graph.append(list(map(int, input().split())))
     for j in range(N):
-        cur=graph[i][j]
-        if cur !=0:
-            coin[cur].append((i,j))
+        if graph[i][j]!=0:
+            data.append((graph[i][j],0,i,j))
 
 
-#print(coin)
-#print(len(coin[0]))
-S,X,Y=  map(int, input().split())              
+target_S, target_X, target_Y = map(int, input().split())
 
-q=deque()
+q=deque(data)
 
-#각코인마다 그 코인의 길이가 0이 아니면 bfs
-q.append((1,1))
+while q:
+    
+    v,s,x,y=q.popleft()
 
+    if s==target_S:
+        print(graph[target_X-1][target_Y-1])
+        break
+        #print(x,y)
+        #print("============")
+    
+    for i in range(4):
+        nx=x+dx[i]
+        ny=y+dy[i]
+        if nx>=0 and nx<N and ny>=0 and ny<N and graph[nx][ny]==0:
+            graph[nx][ny]=v
+            q.append((v,s+1,nx,ny))
+        
+        
 
-for data in coin:
-    if len(data)!=0:
-        print(data)
-        for x in data:
-            if graph[x[0]][x[1]]!=0:
-               
-                for i in range(4):
-                    nx=x[0]+dx[i]
-                    ny=x[1]+dy[i]
-                    if nx>=0 and nx<N and ny>=0 and ny<N and graph[nx][ny]==0:
-                        graph[nx][ny]=graph[x[0]][x[1]]
-                        q.append((nx,ny))
-'''
-
-
- 
-                     
-
-
-
-
-
-
-#print("z")
+             
 
