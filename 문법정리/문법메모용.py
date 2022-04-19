a3c1eae0c091da1d1a13f189a27cc5af75f9cd1d
zero2755 <56134028+zero2755@users.noreply.github.com>
2022-04-19 21:48:47 +0900

    Create 문법메모용.py

---
+++ 문법정리/문법메모용.py
@@ -0,0 +1,30 @@
+#이런방식으론 내가원하는 인접리스트 못만듬
+#graph=[[]*(N+1)]
+#test=[0]*N 이런식으로 사용하는 거임
+graph=[[] for i in range(N+1)]
+print(graph)
+
+```
+compressed += str(count) + prev if count >= 2 else prev
+```
+
+```
+print("5".isalpha())
+print("5".isdigit())
+
+resultArr=['5','4','3',"b",'1']
+print("".join(resultArr))
+#543b1
+print("zxc".join(resultArr))
+#5zxc4zxc3zxcbzxc1
+
+==================================================================
+my="zzzz"
+count=1
+prev="prevStr"
+test="xx"+my if count>=2 else prev
+
+print(test) #count1가 1이면 xxprevStr, 카운트가 3이면 xxzzzz
+```
+
+ord(’A’)
